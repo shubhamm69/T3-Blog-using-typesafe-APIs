9af2c55728448a9cc304f9ef8fd07f7f19417a63
Shubham Singh <shubham212a@gmail.com>
2023-09-30 02:15:24 +0530

    implemented UI for Topics

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,8 +21,9 @@ export const Header = () => {
                 />
               </div>
             </label>
-          ) : ""
-          }
+          ) : (
+            ""
+          )}
         </div>
         <button
           className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
